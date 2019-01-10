51697181ca9b91cb658ad3a057800a27f70127b2
shimmiChristo <shimmin.christopher@gmail.com>
2019-01-09 21:14:48 -0500

    completed the to do list

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 
 var task = ['buy food', 'practice nodejs'];
 
@@ -12,8 +13,23 @@ app.post('/addtask', function(req,res){
     task.push(newTask);
     res.redirect('/');
 });
+var complete = ["finish jquery"];
+app.post('/removetask', function(req,res) {
+    var completeTask = req.body.check;
+    if(typeof completeTask === "string") {
+        complete.push(completeTask);
+        task.splice(task.indexOf(completeTask), 1);
+
+    } else if(typeof completeTask === "object") {
+        for(var i=0; i< completeTask.length; i++) {
+            complete.push(completeTask[i]);
+            task.splice(task.indexOf(completeTask[i]),1);
+        }
+    }
+    res.redirect('/');
+});
 app.get('/', function (req, res) {
-    res.render('index', { task: task});
+    res.render('index', { task: task, complete: complete});
 });
 
 app.listen(3000, function () {
